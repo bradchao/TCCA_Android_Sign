7876ff1ed8a41a3e2131b91116ab2f698793f3e3
bradchao <brad@brad.tw>
2020-08-29 11:04:49 +0800

    2020082904

--- app/src/main/java/tw/org/tcca/apps/sign/SignView.java
+++ app/src/main/java/tw/org/tcca/apps/sign/SignView.java
@@ -1,8 +1,11 @@
 package tw.org.tcca.apps.sign;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -12,5 +15,14 @@ public class SignView extends View {
         super(context, attrs);
 
         setBackgroundColor(Color.YELLOW);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+        paint.setStrokeWidth(10);
+        canvas.drawLine(0,0,100,100, paint);
     }
 }
